8174b1971ae863ca27c6e78af588cec38070af38
Guille <guillermo.c.martinez@dcsl.com>
2024-02-14 10:03:55 +0100

    feat: added user and new user behaviour producers for redis

--- userbehaviorproducer.py
+++ userbehaviorproducer.py
@@ -6,15 +6,16 @@ import datetime
 
 class UserBehaviorProvider(BaseProvider):
     def user_id(self):
-        userIds = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        return random.choice(userIds)
+        user_ids = [x for x in range(1024)]
+        # user_ids = [3023,1135,3817,2344,1309,506,3253,3203,2856,2592,2390]
+        return random.choice(user_ids)
 
     def item_id(self):
-        validIds = [21, 22, 23, 24, 25, 26, 27, 28, 29]
+        validIds = [v for v in range(20, 1024)]
         return random.choice(validIds)
 
     def behavior(self):
-        behaviorNames = ["view", "cart", "buy"]
+        behaviorNames = ["view", "cart", "buy", "clear", "abandon"]
         return random.choice(behaviorNames)
 
     def group_name(self):
@@ -22,17 +23,21 @@ class UserBehaviorProvider(BaseProvider):
         return random.choice(groupNames)
 
     def view_id(self):
-        viewIds = [111, 222, 555]
+        viewIds = [v for v in range(111, 2222, 111)]
         return random.choice(viewIds)
 
-    def produce_msg(self):
+    def produce_msg(self, user_ids = None):
+        user_id = self.user_id()
+        if (user_ids is not None):
+          user_id = random.choice(user_ids)
+      
         ts = time.time() - random.randint(-5, 5)
         b = self.behavior()
         view_id = None
         if b == "view":
             view_id = self.view_id()
         message = {
-            "user_id": self.user_id(),
+            "user_id": user_id,
             "item_id": self.item_id(),
             "behavior": b,
             "view_id": view_id,
